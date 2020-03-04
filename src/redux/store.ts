8d678db3a68f56079deb51c8fb4f1b3dc11b5617
Andrés Pérez Zuluaga - Ceiba Software <andres.zuluaga@ceiba.com.co>
2020-03-03 21:08:38 -0500

    Configura store para uso del devtools de Redux

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,5 +1,21 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from "./reducers";
 import thunk from 'redux-thunk';
 
-export default createStore(rootReducer, applyMiddleware(thunk));
+declare const window: any;
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(rootReducer, composeEnhancers(
+  applyMiddleware(thunk)
+));
+
+export default store;
+
+/*
+export default createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(thunk));
+
+  */
